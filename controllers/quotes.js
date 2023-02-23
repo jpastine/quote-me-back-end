36e3cae81d06e67e53ee957a88378a49e0b5b7b0
justinpastine <justin.pastine@gmail.com>
2023-02-23 14:26:58 -0500

    add update quote functionality

---
+++ controllers/quotes.js
@@ -0,0 +1,44 @@
+const { Quote, Vote } = require('../models')
+
+async function createQuote(req, res) {
+  try {
+    req.body.profileId = req.user.profile.id
+    const quote = await Quote.create(req.body)
+    res.status(200).json(quote)
+  } catch (error) {
+    res.status(500).json({err: error})
+  }
+}
+
+async function index(req, res) {
+  try {
+    const quotes = await Quote.findAll({
+      include: [{ model: Vote, as: 'votesReceived'}]
+    })
+    res.status(200).json(quotes)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({err: error})
+  }
+}
+
+async function editQuote(req, res) {
+  try {
+    const quote = await Quote.update(
+      req.body,
+      { where: {id: req.params.id}, returning:true })
+    res.status(200).json(quote)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({err: error})
+  }
+}
+
+
+
+
+module.exports = {
+  createQuote,
+  index, 
+  editQuote
+}
---
+++ routes/quotes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router()
+const quotesCtrl = require('../controllers/quotes.js')
+const middleware = require('../middleware/auth.js')
+
+const { decodeUserFromToken, checkAuth } = middleware
+
+/*------------Public Routes--------------*/
+
+
+
+/*------------Protected Routes------------*/
+router.use(decodeUserFromToken)
+router.get('/', checkAuth, quotesCtrl.index)
+router.post('/', checkAuth, quotesCtrl.createQuote)
+router.put('/:id', checkAuth, quotesCtrl.editQuote)
+// router.delete('/:id', checkAuth, quotesCtrl)
+
+
+
+module.exports = router 
